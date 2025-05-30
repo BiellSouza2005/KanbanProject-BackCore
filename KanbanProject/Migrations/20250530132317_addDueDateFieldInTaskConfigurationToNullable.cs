6e4cb3aad848e138681b5d6d61f7e4a8c1306a90
Gustavo Alves <gustavoalvesvaladao@gmail.com>
2025-05-30 10:23:54 -0300

    adding due-date in taskConfiguration

---
+++ KanbanProject/Migrations/20250530132317_addDueDateFieldInTaskConfigurationToNullable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KanbanProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class addDueDateFieldInTaskConfigurationToNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
